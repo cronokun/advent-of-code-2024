a955b02c76d6ad0d5bf81252308d889342aca4cd
cronokun <crono.neko@gmail.com>
2025-01-08 21:31:00 +0300

    Day 6, part 2: Looped guard gallivant

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,5 +1,5 @@
--- description: Day 6: Guard Gallivant
-module Day6 (part1) where
+-- Day 6: Guard Gallivant
+module Day6 (part1, part2) where
 
 import Data.List (sort)
 import Data.Set (Set)
@@ -7,43 +7,53 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 
 type Coord = (Integer, Integer)
-data Dir = MDown | MLeft | MRight | MUp
+type Move = (Dir, Integer, (Integer, Integer))
+type ObsMap = Map.Map Integer [Integer]
+data Dir = MDown | MLeft | MRight | MUp deriving (Eq, Show)
 
 part1 :: String -> Int
 part1 input =
   let (limits, start, obs) = parse input
       obs' = prepareMap obs
-   in traverseMap MUp start limits obs' Set.empty
+      path = traverseMap MUp start limits obs' []
+   in Set.size $ expandPath path
 
-move :: Dir -> Coord -> (Map.Map Integer [Integer], Map.Map Integer [Integer]) -> Maybe Coord
+part2 :: String -> Int
+part2 input =
+  let (limits, start, obs) = parse input
+      obs' = prepareMap obs
+      path = Set.toList . expandPath $ traverseMap MUp start limits obs' []
+      validObs = putObsAndTraverse [] start limits obs' path
+  in length validObs
 
+move :: Dir -> Coord -> (ObsMap, ObsMap) -> Maybe Coord
 move MLeft (x, y) (_, rows) =
   let obs' = obstaclesInRow y rows
       obsPos = nextObs (< x) obs'
    in case obsPos of
         [x'] -> Just (x' + 1, y)
-        []   -> Nothing
+        _ -> Nothing
 
 move MRight (x, y) (_, rows) =
   let obs' = reverse $ obstaclesInRow y rows
       obsPos = nextObs (> x) obs'
    in case obsPos of
         [x'] -> Just (x' - 1, y)
-        []   -> Nothing
+        _ -> Nothing
 
 move MUp (x, y) (cols, _) =
   let obs' = obstaclesInCol x cols
       obsPos = nextObs (< y) obs'
    in case obsPos of
         [y'] -> Just (x, y' + 1)
-        []   -> Nothing
+        _ -> Nothing
 
 move MDown (x, y) (cols, _) =
   let obs' = reverse $ obstaclesInCol x cols
       obsPos = nextObs (> y) obs'
    in case obsPos of
         [y'] -> Just (x, y' - 1)
-        []   -> Nothing
+        _ -> Nothing
 
 nextObs :: (Integer -> Bool) -> [Integer] -> [Integer]
 nextObs fun obs = take 1 $ filter fun obs
@@ -54,31 +64,71 @@ obstaclesInCol x cols = Map.findWithDefault [] x cols
 obstaclesInRow :: Ord k => k -> Map.Map k [a] -> [a]
 obstaclesInRow y rows = Map.findWithDefault [] y rows
 
-traverseMap :: Dir -> Coord -> Coord -> (Map.Map Integer [Integer], Map.Map Integer [Integer]) -> Set Coord -> Int
-traverseMap dir pos limits obs acc =
+-- Traverse map until go outside or a loop is detected. If obstacle is met, turn right before the
+-- obstacle and continue traversal.
+traverseMap :: Dir -> Coord -> Coord -> (ObsMap, ObsMap) -> [Move] -> [Move]
+traverseMap dir pos@(x, y) limits@(mx, my) obs acc =
   case move dir pos obs of
-    Just nextPos -> traverseMap (nextDir dir) nextPos limits obs (addVisited pos nextPos acc)
-    Nothing -> Set.size $ addVisited pos (outerPos dir pos limits) acc
+    Just next -> maybeContinue next
+    Nothing -> (movement outerPos):acc
   where
-    addVisited (x, y) (x', y') visited =
-      let [x1, x2] = sort [x, x']
-          [y1, y2] = sort [y, y']
-          positions = [(xx, yy) | xx <- [x1..x2], yy <- [y1..y2]]
-       in foldr Set.insert visited positions
+    movement (x', y') =
+      case dir of
+        MDown -> (dir, x, (y, y'))
+        MUp -> (dir, x, (y, y'))
+        MLeft -> (dir, y, (x, x'))
+        MRight -> (dir, y, (x, x'))
 
-    outerPos MDown  (x, _) (_, my) = (x,  my)
-    outerPos MLeft  (_, y) (_,  _) = (1,  y)
-    outerPos MRight (_, y) (mx, _) = (mx, y)
-    outerPos MUp    (x, _) (_,  _) = (x,  1)
+    maybeContinue p =
+      case elem (movement p) acc of
+        True -> []  -- It's a loop! Abort!
+        False -> traverseMap nextDir p limits obs ((movement p):acc)
 
-    nextDir direction =
-      case direction of
+    outerPos =
+      case dir of
+        MDown -> (x, my)
+        MUp -> (x, 1)
+        MLeft -> (1, y)
+        MRight -> (mx, y)
+
+    nextDir =
+      case dir of
         MDown  -> MLeft
         MLeft  -> MUp
         MRight -> MDown
         MUp    -> MRight
 
-prepareMap :: [Coord] -> (Map.Map Integer [Integer], Map.Map Integer [Integer])
+expandPath :: [Move] -> Set Coord
+expandPath moves = reduce Set.empty moves
+  where
+    reduce acc [] = acc
+    reduce acc (m:ms) =
+      let acc' = foldr Set.insert acc (moveToList m)
+      in reduce acc' ms
+
+    moveToList  (MDown, x, (y1, y2)) = [(x, y) | y <- [y1..y2]]
+    moveToList    (MUp, x, (y1, y2)) = [(x, y) | y <- [y2..y1]]
+    moveToList  (MLeft, y, (x1, x2)) = [(x, y) | x <- [x2..x1]]
+    moveToList (MRight, y, (x1, x2)) = [(x, y) | x <- [x1..x2]]
+
+
+-- Put a new obstacle on the path and try to traverse. Increase acc if loop detected.
+putObsAndTraverse :: [Coord] -> Coord -> Coord -> (ObsMap, ObsMap) -> [Coord] -> [Coord]
+putObsAndTraverse acc _ _ _ [] = acc
+
+putObsAndTraverse acc start limits obs@(cols, rows) (p:path) =
+  let obs' = putNewObs p
+      newPath = traverseMap MUp start limits obs' []
+      acc' = if newPath == [] then (p:acc) else acc
+   in putObsAndTraverse acc' start limits obs path
+  where
+    putNewObs (x, y) =
+      (putInMap x [y] cols, putInMap y [x] rows)
+    putInMap = Map.insertWith (\v vs -> reverse . sort $ v <> vs)
+
+
+-- Convert a list of obstacles into Maps: obstacles by columns and by rows.
+prepareMap :: [Coord] -> (ObsMap, ObsMap)
 prepareMap obs = helper Map.empty Map.empty obs
   where
     helper cols rows [] = (cols, rows)
@@ -90,8 +140,8 @@ prepareMap obs = helper Map.empty Map.empty obs
 parse :: String -> (Coord, Coord, [Coord])
 parse input = doParse (1, 1) [] (1, 1) input
   where
-    doParse start  obs (x, y) ""          = ((x - 1, y), start, Data.List.sort obs)
-    doParse start  obs (x, y) ('.':rest)  = doParse start obs (x + 1, y) rest
+    doParse start  obs (x, y) []          = ((x - 1, y), start, Data.List.sort obs)
     doParse _start obs (x, y) ('^':rest)  = doParse (x, y) obs (x + 1, y) rest
     doParse start  obs (x, y) ('#':rest)  = doParse start ((x,y):obs) (x + 1, y) rest
     doParse start  obs (_, y) ('\n':rest) = doParse start obs (1, y + 1) rest
+    doParse start  obs (x, y) (_:rest)  = doParse start obs (x + 1, y) rest
--- test/Day6Spec.hs
+++ test/Day6Spec.hs
@@ -23,8 +23,14 @@ spec :: Spec
 spec = do
   describe "part1" $ do
     it "returns number of disctint positions the guard will visit" $ do
-      shouldBe 41 (part1 example1)
+      shouldBe (part1 example1) 41
+
+  describe "part2" $ do
+    it "returns how many different positions could you choose to create loops" $ do
+      shouldBe (part2 example1) 6
 
   describe "answers" $ do
     it "for part 1" $ do
-      shouldBe 5145 (part1 input)
+      shouldBe (part1 input) 5145
+    it "for part 2" $ do
+      shouldBe (part2 input) 1523
