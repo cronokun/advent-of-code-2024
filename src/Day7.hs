4c6d5c77a265ca22edceebc55c9721f5e80f0a7c
cronokun <crono.neko@gmail.com>
2025-01-09 21:38:58 +0300

    Refactoring Day 7

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,50 +1,39 @@
 -- Day 7: Bridge Repair
 module Day7 (part1, part2) where
 
+import Data.List (uncons)
 import Helpers (splitOn)
 
 type Calibration = (Integer, [Integer])
+type Operation = (Integer -> Integer -> Integer)
 
 -- Total calibration result with two operations: (+) and (*).
 part1 :: String -> Integer
-part1 input = sumCorrect allResults $ parse input
+part1 input = sumCorrect [(+), (*)] $ parse input
 
 -- Total calibration result with three operations: (+), (*), and (||).
 part2 :: String -> Integer
-part2 input = sumCorrect allResults' $ parse input
-
-sumCorrect :: ([Integer] -> [Integer]) -> [Calibration] -> Integer
-sumCorrect f calibrations = sum . map fst . filter (isCorrect f) $ calibrations
-
-
--- Check if an equations is correct, i.e. returns expected result.
-isCorrect :: ([Integer] -> [Integer]) -> Calibration -> Bool
-isCorrect f (exp, nums) = any (== exp) (f nums)
-
--- Calculates results for all possible combinations of (+) and (*) operators.
-allResults :: [Integer] -> [Integer]
-allResults (num:rest) = calcResults [num] rest
+part2 input = sumCorrect [(+), (*), concatenate] $ parse input
   where
-    calcResults acc [] = acc
-    calcResults acc (n:ns) =
-      let acc1 = map (+ n) acc
-          acc2 = map (* n ) acc
-          acc' = acc1 <> acc2
-       in calcResults acc' ns
-
--- Calculates results for all possible combinations of (+), (*) and (||) operators.
-allResults' :: [Integer] -> [Integer]
-allResults' (num:rest) = calcResults [num] rest
-  where
-    calcResults acc [] = acc
-    calcResults acc (n:ns) =
-      let acc1 = map (+ n) acc
-          acc2 = map (* n ) acc
-          acc3 = map (flip concatenate n) acc
-          acc' = acc1 <> acc2 <> acc3
-       in calcResults acc' ns
-
+    concatenate :: Integer -> Integer -> Integer
     concatenate a b = read (show a <> show b) :: Integer
+
+sumCorrect :: [Operation] -> [Calibration] -> Integer
+sumCorrect ops calibrations = sum . map fst . filter isCorrect $ calibrations
+  where
+    -- Check if an equations is correct, i.e. returns expected result.
+    isCorrect (expected, nums) =
+      let Just (n,ns) = uncons nums
+      in any (== expected) (calcResults [n] ns)
+
+    -- Return result of calculation with all variants of the given operations.
+    calcResults acc [] = acc
+    calcResults acc (n:ns) =
+      let acc' = concat $ map (applyOpToAll n acc) ops
+       in calcResults acc' ns
+
+    applyOpToAll n ms op = map (\m -> op m n) ms
+
 
 -- Parse input data
 parse :: String -> [Calibration]
