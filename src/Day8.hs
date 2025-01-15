de6e13efdf8af93331a33bf34037e79c6cb8014a
cronokun <crono.neko@gmail.com>
2025-01-15 20:56:38 +0300

    Day 8, part 2: Harmonic resonant collinearity

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,5 +1,5 @@
 -- Day 8: Resonant Collinearity
-module Day8 (part1) where
+module Day8 (part1, part2) where
 
 import qualified Data.List as List
 import qualified Data.Set as Set
@@ -10,31 +10,52 @@ type Coord = (Integer, Integer)
 part1 :: String -> Integer
 part1 input =
   let (m, size) = parse input
-   in toInteger $ calcAntinodes size m
+   in toInteger $ calcAntinodes antinodes size m
+
+-- Part 2: How many unique locations within the bounds of the map contain an harmonic antinode?
+part2 :: String -> Integer
+part2 input =
+  let (m, size) = parse input
+   in toInteger $ calcAntinodes antinodes' size m
+
+-- Return two or less antinodes for two given antennas.
+antinodes :: Coord -> [Coord] -> [Coord]
+antinodes size [(x1,y1), (x2,y2)] =
+  let dx = x2 - x1
+      dy = y2 - y1
+      a1 = (x1 - dx, y1 - dy)
+      a2 = (x2 + dx, y2 + dy)
+   in filter (isInside size) [a1, a2]
+
+-- Return all harmonic antinodes for two given antennas.
+antinodes' :: Coord -> [Coord] -> [Coord]
+antinodes' size [(x1, y1), (x2, y2)] =
+  let dx = x2 - x1
+      dy = y2 - y1
+      as1 = antinodesFrom (+) (dx, dy) (x1, y1)
+      as2 = antinodesFrom (-) (dx, dy) (x1, y1)
+   in as1 <> as2
+  where
+    antinodesFrom f delta point = takeWhile (isInside size) . iterate (vecApply f delta) $ point
+    vecApply f (dx, dy) (x, y) = (f x dx, f y dy)
+
+-- Check if node is inside map.
+isInside :: Coord -> Coord -> Bool
+isInside (mx, my) (x, y) = x > 0 && x <= mx && y > 0 && y <= my
 
 -- Recursively calculate all antinodes.
-calcAntinodes :: Coord -> [(Char, Coord)] -> Int
-calcAntinodes (mx, my) m = helper Set.empty m
+calcAntinodes :: (Coord -> [Coord] -> [Coord]) -> Coord -> [(Char, Coord)] -> Int
+calcAntinodes f size m = helper Set.empty m
   where
     helper acc [] = Set.size acc
     helper acc xs =
       let (fs, rest) = splitByKey xs
           ps = pairs $ map snd fs
-          anti = concatMap antinodes ps
+          anti = concatMap (f size) ps
           acc' = Set.union (Set.fromList anti) acc
        in helper acc' rest
 
     isSameKey (k1, _) (k2, _) = k1 == k2
-
-    antinodes [(x1,y1), (x2,y2)] =
-      let dx = x2 - x1
-          dy = y2 - y1
-          a1 = (x1 - dx, y1 - dy)
-          a2 = (x2 + dx, y2 + dy)
-       in filter isInside [a1, a2]
-
-    isInside (x, y) = x > 0 && x <= mx && y > 0 && y <= my
-
     pairs = filter ((2 == ) . length) . List.subsequences
 
     -- Take all elements with the same key and the rest of the list.
--- test/Day8Spec.hs
+++ test/Day8Spec.hs
@@ -23,7 +23,15 @@ spec = do
     it "returns how many uniq locations contain antinodes" $ do
       shouldBe (part1 example1) 14
 
+  describe "part2" $ do
+    it "returns how many uniq locations contain harmonic antinodes" $ do
+      shouldBe (part2 example1) 34
+
   describe "answers" $ do
     it "for part 1" $ do
       input <- readFile "inputs/8_antennas_map"
       shouldBe (part1 input) 220
+
+    it "for part 2" $ do
+      input <- readFile "inputs/8_antennas_map"
+      shouldBe (part2 input) 813
