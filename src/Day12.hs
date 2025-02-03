a62bf3f392f5c3c5adceb2dbd58c977ae54563a7
cronokun <crono.neko@gmail.com>
2025-02-03 17:18:09 +0300

    Day 12, part 2: Garden groups

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,10 +1,13 @@
 -- Day 12: Garden Groups
-module Day12 (part1) where
+module Day12 (part1, part2) where
 
 import Data.List ((\\))
+import qualified Data.List as List
 import Data.Tuple (swap)
-import Data.Map (Map, findWithDefault, fromList)
+import Data.Map (Map)
+import qualified Data.Map as Map
 
+data Side = SDown | SLeft | SRight | SUp deriving (Eq, Ord, Show)
 type Coord = (Int, Int)
 type PlotMap = Map Coord Char
 type Plot = (Char, Coord)
@@ -13,9 +16,17 @@ type Plot = (Char, Coord)
 part1 :: String -> Int
 part1 input =
   let plots = parse input
-      gmap = fromList $ map swap plots
+      gmap = Map.fromList $ map swap plots
       groups = splitIntoGroups gmap plots
    in sum . map (areaCost gmap) $ groups
+
+--  Returns total price of fencing all regions on the map with bulk discount.
+part2 :: String -> Int
+part2 input =
+  let plots = parse input
+      gmap = Map.fromList $ map swap plots
+      groups = splitIntoGroups gmap plots
+   in sum . map (areaCost' gmap) $ groups
 
 areaCost :: PlotMap -> [Plot] -> Int
 areaCost gmap grp =
@@ -27,6 +38,48 @@ areaCost gmap grp =
     getPerimeter acc ((p, c):ps) = 
       let ns = neigboursWith gmap (/= p) c
        in getPerimeter (acc + length ns) ps
+
+areaCost' :: PlotMap -> [Plot] -> Int
+areaCost' gmap grp =
+  let area = length grp
+      sides = getGroupSides grp
+   in area * sides
+  where
+    getGroupSides :: [Plot] -> Int
+    getGroupSides g =
+      let perimeter = getNeigbourFences [] g
+          grouped = foldr groupBySide Map.empty perimeter
+          lists = map (List.nub . List.sort . snd) $ Map.toList grouped
+       in sum $ map conseqSidesCount lists
+
+    getNeigbourFences :: [(Side, Coord)] -> [Plot] -> [(Side, Coord)]
+    getNeigbourFences acc [] = acc
+    getNeigbourFences acc ((pln, coord@(x, y)):rest) =
+      let ns = neigboursWith gmap (/= pln) coord
+          ns' = map withSide ns
+       in getNeigbourFences (ns' <> acc) rest
+      where
+        withSide (_, (x', y')) =
+          let side = case (x - x', y - y') of
+                       (1,0) -> SLeft
+                       (-1,0) -> SRight
+                       (0,1) -> SUp
+                       (0,-1) -> SDown
+          in (side, coord)
+
+    conseqSidesCount :: [Int] -> Int 
+    conseqSidesCount [] = 0
+    conseqSidesCount xs =
+      let ixs = List.findIndices notSeq $ zip xs (drop 1 xs)
+       in length ixs + 1 -- plus one for last/single element
+      where
+        notSeq (a, b) = abs (a - b) /= 1
+
+    groupBySide :: (Side, Coord) -> Map (Side, Int) [Int] -> Map (Side, Int) [Int]
+    groupBySide (SUp,    (x, y)) m  = Map.insertWith (<>) (SUp,    y) [x] m
+    groupBySide (SDown,  (x, y)) m  = Map.insertWith (<>) (SDown,  y) [x] m
+    groupBySide (SLeft,  (x, y)) m  = Map.insertWith (<>) (SLeft,  x) [y] m
+    groupBySide (SRight, (x, y)) m  = Map.insertWith (<>) (SRight, x) [y] m
 
 splitIntoGroups :: PlotMap -> [Plot] -> [[Plot]]
 splitIntoGroups gmap plots = doSplit [] plots
@@ -51,7 +104,7 @@ neigboursWith gmap f (x,y) =
    in filter (f . fst) ns
   where
     mapper c =
-      let p = findWithDefault ' ' c gmap
+      let p = Map.findWithDefault ' ' c gmap
        in (p, c)
 
 parse :: String -> [Plot]
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -3,7 +3,7 @@ module Day12Spec (spec) where
 import Day12
 import Test.Hspec hiding (example)
 
-example1, example2, example3 :: String
+example1, example2, example3, example4, example5 :: String
 
 example1 = "AAAA\n\
            \BBCD\n\
@@ -27,6 +27,19 @@ example3 = "RRRRIICCFF\n\
            \MIIISIJEEE\n\
            \MMMISSJEEE\n"
 
+example4 = "EEEEE\n\
+           \EXXXX\n\
+           \EEEEE\n\
+           \EXXXX\n\
+           \EEEEE\n"
+
+example5 = "AAAAAA\n\
+           \AAABBA\n\
+           \AAABBA\n\
+           \ABBAAA\n\
+           \ABBAAA\n\
+           \AAAAAA\n"
+
 spec :: Spec
 spec = do
   describe "part1" $ do
@@ -35,7 +48,19 @@ spec = do
       (part1 example2) `shouldBe` 772
       (part1 example3) `shouldBe` 1930
 
+  describe "part2" $ do
+    it "returns total price of fencing all regions with bulk discount" $ do
+      (part2 example1) `shouldBe` 80
+      (part2 example2) `shouldBe` 436
+      (part2 example3) `shouldBe` 1206
+      (part2 example4) `shouldBe` 236
+      (part2 example5) `shouldBe` 368
+
   describe "answers" $ do
     it "for part 1" $ do
       input <- readFile "inputs/12_garden_plots_map"
       (part1 input) `shouldBe` 1473408
+
+    it "for part 2" $ do
+      input <- readFile "inputs/12_garden_plots_map"
+      (part2 input) `shouldBe` 886364
