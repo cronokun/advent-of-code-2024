009994b72bd42ecf2c23bc5042fe9e8ae0166eed
cronokun <crono.neko@gmail.com>
2025-03-12 15:29:02 +0300

    Day 17, part 2: Chronospatial computer

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE RecordWildCards #-}
 
 -- Day 17: Chronospatial Computer
-module Day17 (part1) where
+module Day17 (part1, part2) where
 
 import Data.Bits (xor)
-import Data.List ((!?), intercalate)
+import Data.List ((!?), intercalate, nub)
+
+---- Computer implementation
 
 data Computer = Computer
   { regA :: Int
@@ -16,33 +18,16 @@ data Computer = Computer
   , halt :: Int
   } deriving (Show)
 
--- Returns the output debug string.
-part1 :: String -> String
-part1 input =
-  let info = parse input
-      comp = buildComputer info
-   in formatOutput . output . runProgram $ comp
-  where
-    formatOutput = intercalate "," . map show . reverse
-
-emptyComputer :: Computer
-emptyComputer = Computer { regA = 0
-                         , regB = 0
-                         , regC = 0
-                         , output = []
-                         , program = []
-                         , pointer = 0
-                         , halt = 0
-                         }
-
 buildComputer :: (Int, Int, Int, [Int]) -> Computer
 buildComputer (a, b, c, prg) =
-  emptyComputer { regA = a
-                , regB = b
-                , regC = c
-                , program = prg
-                , halt = length prg
-                }
+  Computer { regA = a
+           , regB = b
+           , regC = c
+           , output = []
+           , program = prg
+           , pointer = 0
+           , halt = length prg
+           }
 
 runProgram :: Computer -> Computer
 runProgram comp@Computer{..} =
@@ -110,6 +95,72 @@ runProgram comp@Computer{..} =
         6 -> regC
 
     litop = program !! (pointer + 1)
+
+---- Solutions
+
+-- Returns the output debug string.
+part1 :: String -> String
+part1 input =
+  let info = parse input
+      comp = buildComputer info
+   in formatOutput . output . runProgram $ comp
+  where
+    formatOutput = intercalate "," . map show . reverse
+
+-- Returns lowest positive initial value for register A that make program to output a copy of itself.
+part2 :: String -> Int
+part2 input =
+  let info = parse input
+      comp = buildComputer info
+      candidates = narrowdown 0 (length $ program comp) [] comp
+   in minimum candidates
+
+narrowdown :: Int -> Int -> [[Int]] -> Computer -> [Int]
+narrowdown cur len condidates comp
+  | len == cur = bitsToNums condidates
+  | otherwise =
+  let expected = drop (len - cur - 1) $ program comp
+      ns = filter (\x -> getOutputFrom x  == expected) $ expand condidates
+      condidates' = unify ns
+   in narrowdown (cur + 1) len condidates' comp
+  where
+    getOutputFrom :: Int -> [Int]
+    getOutputFrom a = reverse . output . runProgram $ comp { regA = a }
+
+    expand :: [[Int]] -> [Int]
+    expand xs = bitsToNums (xs <> [[0..7]])
+
+    unify :: [Int] -> [[Int]]
+    unify [0,1,2,3,4,5,6,7] = [[0..7]]
+    unify xs = reduce [] . map intToOct $ xs
+      where
+        takeHeads = foldr (\(a:b) (as,bs)  -> ((a:as), (b:bs))) ([],[])
+
+        reduce acc ns
+          | all null ns = reverse acc
+          | otherwise =
+            let (hs, ns') = takeHeads ns
+             in reduce (nub hs : acc) ns'
+
+    bitsToNums :: [[Int]] -> [Int]
+    bitsToNums xs = helper (zip [0..] (reverse xs)) [0]
+      where
+        applicativeSum :: [Int] -> [Int] -> [Int]
+        applicativeSum as bs = (map (+) bs) <*> as
+
+        helper :: [(Int, [Int])] -> [Int] -> [Int]
+        helper [] acc = acc
+        helper ((p, ns):rest) acc =
+          let ns' = map (\n -> (8 ^ p) * n) ns
+              acc' = applicativeSum acc ns'
+           in helper rest acc'
+
+    intToOct :: Int -> [Int]
+    intToOct n = helper n []
+      where
+        helper x acc
+          | x == 0 = acc
+          | otherwise = helper (x `div` 8) ((x `rem` 8) : acc)
 
 parse :: String -> (Int, Int, Int, [Int])
 parse input =
--- test/Day17Spec.hs
+++ test/Day17Spec.hs
@@ -1,22 +1,37 @@
 module Day17Spec (spec) where
 
 import Day17
-import Test.Hspec hiding (example)
+import Test.Hspec
 
-example :: String
-example = "Register A: 729\n\
+example1, example2 :: String
+
+example1 = "Register A: 729\n\
           \Register B: 0\n\
           \Register C: 0\n\
           \\n\
           \Program: 0,1,5,4,3,0\n"
 
+example2 = "Register A: 2024\n\
+           \Register B: 0\n\
+           \Register C: 0\n\
+           \\n\
+           \Program: 0,3,5,4,3,0\n"
+
 spec :: Spec
 spec = do
   describe "part1" $ do
     it "Returns the output debug string" $ do
-      (part1 example) `shouldBe` "4,6,3,5,6,3,5,2,1,0"
+      (part1 example1) `shouldBe` "4,6,3,5,6,3,5,2,1,0"
+
+  describe "part2" $ do
+    it "Returns lowest positive initial value for register A that make program to output a copy of itself" $ do
+      (part2 example2) `shouldBe` 117440
 
   describe "answers" $ do
     it "for part1" $ do
       input <- readFile "inputs/17_program_info"
       (part1 input) `shouldBe` "1,3,5,1,7,2,5,1,6"
+
+    it "for part2" $ do
+      input <- readFile "inputs/17_program_info"
+      (part2 input) `shouldBe` 236555997372013
