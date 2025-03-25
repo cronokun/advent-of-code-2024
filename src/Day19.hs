4656502b3490766b62c6ba4bc5bd71b756c964bd
cronokun <crono.neko@gmail.com>
2025-03-25 13:18:24 +0300

    Refactoring Day19: ditch Text in favour of more speedy Data.List

--- src/Day19.hs
+++ src/Day19.hs
@@ -6,40 +6,49 @@ module Day19 (part1, part2) where
 import Data.Maybe (fromJust, isJust)
 import qualified Data.List as L
 import qualified Data.Map as M
-import qualified Data.Text as T
+import Helpers (lineGroups)
 
 -- Returns number of possible towel designs.
 part1 :: String -> Int
 part1 input =
   let (patterns, designs) = parse input
-      patterns' = reverse $ L.sortOn T.length patterns 
-   in length . filter (> 0) . map (possiblePatternsFor patterns') $ designs
+   in length . filter (isPossible patterns) $ designs
+  where
+    isPossible :: [String] -> String -> Bool
+    isPossible patterns design = helper [design]
+      where
+        helper xs =
+          case stripPatternsFrom xs of
+            [] -> False
+            rs -> if any (== "") rs then True else helper rs
+
+        stripPatternsFrom :: [String] -> [String]
+        stripPatternsFrom xs = L.nub [fromJust s | d <- xs, p <- patterns, let s = L.stripPrefix p d, isJust s]
 
 -- Returns sum of number of different ways you could make each design.
 part2 :: String -> Int
 part2 input =
   let (patterns, designs) = parse input
-      patterns' = reverse $ L.sortOn T.length patterns 
-   in sum . map (possiblePatternsFor patterns') $ designs
-
-possiblePatternsFor :: [T.Text] -> T.Text -> Int
-possiblePatternsFor patterns design = helper [(design, 1)]
+   in sum . map (possiblePatternsFor patterns) $ designs
   where
-    helper xs = case M.toList $ doStrip M.empty xs of
-                  [] -> 0
-                  [("", n)] -> n
-                  xs' -> helper xs'
+    possiblePatternsFor :: [String] -> String -> Int
+    possiblePatternsFor patterns design = helper [(design, 1)]
+      where
+        helper xs = case doStrip M.empty xs of
+                      [] -> 0
+                      [("", n)] -> n
+                      xs' -> helper xs'
 
-    doStrip acc [] = acc
-    doStrip acc (("", k) : xs) = doStrip (M.insert "" k acc) xs
-    doStrip acc ((d, k) : xs) =
-      let striped = [(fromJust s, k) | p <- patterns, let s = T.stripPrefix p d, isJust s]
-          acc' = foldr (\(key, val) m -> M.insertWith (+) key val m) acc striped
-      in doStrip acc' xs
+        doStrip :: M.Map String Int -> [(String, Int)] -> [(String, Int)]
+        doStrip acc [] = M.toList acc
+        doStrip acc (("", k) : xs) = doStrip (M.insert "" k acc) xs
+        doStrip acc ((d, k) : xs) =
+          let striped = [(fromJust s, k) | p <- patterns, let s = L.stripPrefix p d, isJust s]
+              acc' = foldr (\(key, val) m -> M.insertWith (+) key val m) acc striped
+          in doStrip acc' xs
 
-parse :: String -> ([T.Text], [T.Text])
+parse :: String -> ([String], [String])
 parse input =
-  let [ps, ds] = T.splitOn "\n\n" (T.pack input)
-      patterns = T.splitOn ", " ps
-      designs = T.lines ds
+  let [[ps], designs] = lineGroups input
+      patterns = map (L.delete ',') $ words ps
    in (patterns, designs)
