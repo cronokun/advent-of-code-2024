2af710c891d26785895a8dfff3569df2953fcc75
cronokun <crono.neko@gmail.com>
2025-04-24 16:32:51 +0300

    Day 22, part 2: Monkey market

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,18 +1,77 @@
 -- Day 22: Monkey Market
-module Day22 where
+module Day22 (part1, part2) where
 
 import Data.Bits ((.&.), shift, xor)
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+type Memo = M.Map Int Int
 
 part1 :: String -> Int
-part1 input = sum . map (secretNumber 2000) . parse $ input
+part1 input = sum . map (secretNumberAt 2000) . parse $ input
 
-secretNumber :: Int -> Int -> Int
-secretNumber 0 x = x
-secretNumber n x =
-  let x' = calc 11 . calc (-5) . calc 6 $ x
-   in secretNumber (n - 1) x'
+part2 :: String -> Int
+part2 input = topPrice $ parse input
+
+secretNumberAt :: Int -> Int -> Int
+secretNumberAt 0 x = x
+secretNumberAt n x = secretNumberAt (n - 1) $ nextSecretNumber x
+
+nextSecretNumber :: Int -> Int
+nextSecretNumber num = calc 11 . calc (-5) . calc 6 $ num
   where
     calc n x = ((x `shift` n) `xor` x) .&. 16777215
+
+topPrice :: [Int] -> Int
+topPrice xs = run M.empty xs
+  where
+    run :: Memo -> [Int] -> Int
+    run memo [] = maximum $ M.elems memo
+    run memo (x:xs) =
+      let res = sequentize $ priceAndChanges 2000 x
+          acc' = foldl (\m (p, k) -> M.insertWith (+) k p m) memo res
+       in run acc' xs
+
+priceAndChanges :: Int -> Int -> [(Int, Int)]
+priceAndChanges n num = run [(price num, 0)] n num
+  where
+    run acc 0 _ = reverse acc
+    run acc@((p, _) : _) n' x =
+      let x' = nextSecretNumber x
+          p' = price x'
+      in run ((p', p' - p) : acc) (n' - 1) x'
+
+sequentize :: [(Int, Int)] -> [(Int, Int)]
+sequentize xs =
+  let (as, xs') = splitAt 4 . drop 1 $ xs
+      p = fst $ last as
+      seq = tuple4 as
+      key = toKey seq
+  in run [(p, key)] S.empty seq xs'
+  where
+    run acc s prev [] = reverse acc
+    run acc s prev ((p, c) : xs) =
+      let seq = shift4 prev c
+          key = toKey seq
+          (acc', s') = if S.member key s
+                          then (acc, s)
+                          else ((p, key) : acc, S.insert key s)
+       in run acc' s' seq xs
+
+    shift4 (_, a, b, c) x = (a, b, c, x)
+    tuple4 [(_, a), (_, b), (_, c), (_, d)] = (a, b, c, d)
+
+toKey (a, b, c, d) =
+  let a' = (a + 9) `shift` 15
+      b' = (b + 9) `shift` 10
+      c' = (c + 9) `shift` 5
+      d' = (d + 9)
+   in a' + b' + c' + d'
+
+
+price :: Int -> Int
+price x = x `rem` 10
 
 parse :: String -> [Int]
 parse = map (\x -> read x :: Int) . lines
--- test/Day22Spec.hs
+++ test/Day22Spec.hs
@@ -3,16 +3,27 @@ module Day22Spec (spec) where
 import Day22
 import Test.Hspec hiding (example)
 
-example :: String
-example = "1\n10\n100\n2024"
+example1 :: String
+example1 = "1\n10\n100\n2024"
+
+example2 :: String
+example2 = "1\n2\n3\n2024"
 
 spec :: Spec
 spec = do
   describe "part1" $ do
-    it "return sum of 2000th secret numbers" $ do
-      part1 example `shouldBe` 37327623
+    it "returns sum of 2000th secret numbers" $ do
+      part1 example1 `shouldBe` 37327623
+
+  describe "part2" $ do
+    it "returns maximum number of bananas" $ do
+      part2 example2 `shouldBe` 23
 
   describe "answers" $ do
     it "for part 1" $ do
       input <- readFile "inputs/22_secret_numbers"
       part1 input `shouldBe` 20506453102
+
+    it "for part 2" $ do
+      input <- readFile "inputs/22_secret_numbers"
+      part2 input `shouldBe` 2423
