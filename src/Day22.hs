ab841bb36b0ae0986fd3716e43f8cc44e8fef2e0
cronokun <crono.neko@gmail.com>
2025-04-24 17:34:49 +0300

    Refactoring: optimize Day 22

--- src/Day22.hs
+++ src/Day22.hs
@@ -2,21 +2,17 @@
 module Day22 (part1, part2) where
 
 import Data.Bits ((.&.), shift, xor)
-import qualified Data.List as L
-import qualified Data.Map as M
-import qualified Data.Set as S
+import qualified Data.IntMap.Lazy as M
 
-type Memo = M.Map Int Int
+type Memo = M.IntMap Int
 
 part1 :: String -> Int
-part1 input = sum . map (secretNumberAt 2000) . parse $ input
+part1 input = sum . map (secretNumberAt 2000) $ parse input
+  where
+    secretNumberAt n x = iterate nextSecretNumber x !! n
 
 part2 :: String -> Int
 part2 input = topPrice $ parse input
-
-secretNumberAt :: Int -> Int -> Int
-secretNumberAt 0 x = x
-secretNumberAt n x = secretNumberAt (n - 1) $ nextSecretNumber x
 
 nextSecretNumber :: Int -> Int
 nextSecretNumber num = calc 11 . calc (-5) . calc 6 $ num
@@ -29,39 +25,21 @@ topPrice xs = run M.empty xs
     run :: Memo -> [Int] -> Int
     run memo [] = maximum $ M.elems memo
     run memo (x:xs) =
-      let res = sequentize $ priceAndChanges 2000 x
-          acc' = foldl (\m (p, k) -> M.insertWith (+) k p m) memo res
-       in run acc' xs
+      let m = priceForSequence x
+          memo' = M.unionWith (+) memo m
+       in run memo' xs
 
-priceAndChanges :: Int -> Int -> [(Int, Int)]
-priceAndChanges n num = run [(price num, 0)] n num
+priceForSequence :: Int -> Memo
+priceForSequence x =
+  let xs = take 2000 . map (`mod` 10) . iterate nextSecretNumber $ x
+   in M.fromListWith (const id) $ groupBy4 xs
   where
-    run acc 0 _ = reverse acc
-    run acc@((p, _) : _) n' x =
-      let x' = nextSecretNumber x
-          p' = price x'
-      in run ((p', p' - p) : acc) (n' - 1) x'
+    groupBy4 (a : b : c : d : e : xs) =
+      let delta = toKey (b - a, c - b, d - c, e - d)
+       in (delta, e) : groupBy4 (b : c : d : e : xs)
+    groupBy4 _ = []
 
-sequentize :: [(Int, Int)] -> [(Int, Int)]
-sequentize xs =
-  let (as, xs') = splitAt 4 . drop 1 $ xs
-      p = fst $ last as
-      seq = tuple4 as
-      key = toKey seq
-  in run [(p, key)] S.empty seq xs'
-  where
-    run acc s prev [] = reverse acc
-    run acc s prev ((p, c) : xs) =
-      let seq = shift4 prev c
-          key = toKey seq
-          (acc', s') = if S.member key s
-                          then (acc, s)
-                          else ((p, key) : acc, S.insert key s)
-       in run acc' s' seq xs
-
-    shift4 (_, a, b, c) x = (a, b, c, x)
-    tuple4 [(_, a), (_, b), (_, c), (_, d)] = (a, b, c, d)
-
+toKey :: (Int, Int, Int, Int) -> Int
 toKey (a, b, c, d) =
   let a' = (a + 9) `shift` 15
       b' = (b + 9) `shift` 10
