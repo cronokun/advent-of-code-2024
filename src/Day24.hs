9ecb3efdf57a147a62e485a8d37a924cadb1b420
cronokun <crono.neko@gmail.com>
2025-05-03 12:19:40 +0300

    Day 24, part 1: Crossed wires

---
+++ src/Day24.hs
@@ -0,0 +1,89 @@
+-- Day 24: Crossed Wires
+module Day24 (part1, part2) where
+
+import Data.Map (Map)
+import qualified Data.Map as M
+import qualified Data.List as L
+import Helpers (lineGroups, splitOnL)
+
+data Gate = AND | OR | XOR deriving (Eq, Show)
+
+data Device = Device
+  { connections :: [Connection]
+  , wires :: Wires
+  } deriving (Show)
+
+data Connection = Connection
+  { gate :: Gate
+  , input :: (String, String)
+  , output :: String
+  } deriving (Show)
+
+type Wires = Map String Bool
+
+-- Returns number on the Z-wires.
+part1 :: String -> Int
+part1 input = toDecimal . zWires . run $ parse input
+
+-- TBD
+part2 :: String -> Int
+part2 input = const 0 $ parse input
+
+run :: Device -> Device
+run Device { connections = cs, wires = w } =
+  let w' = foldr (\c s -> snd $ doRun c s) w cs
+   in Device { connections = cs, wires = w' }
+  where
+    doRun :: Connection -> Wires -> (Bool, Wires)
+    doRun (Connection { gate = g, input = (a, b), output = o }) s =
+      let (va, s1) = getValueFor a s
+          (vb, s2) = getValueFor b s1
+          res = op g va vb
+          s' = M.insert o res s2
+       in (res, s')
+
+    getValueFor :: String -> Wires -> (Bool, Wires)
+    getValueFor x s =
+      case M.lookup x s of
+        Just v -> (v, s)
+        Nothing ->
+          let Just c = L.find (\(Connection { output = o }) -> o == x) cs
+           in doRun c s
+
+    op :: Gate -> Bool -> Bool -> Bool
+    op AND a b = a && b
+    op OR a b = a || b
+    op XOR a b = a /= b
+
+toDecimal :: [Bool] -> Int
+toDecimal = run 0 0
+  where run acc _ [] = acc
+        run acc n (True : xs) = run (acc + 2^n) (n + 1) xs
+        run acc n (False : xs) = run acc (n + 1) xs
+
+zWires :: Device -> [Bool]
+zWires d = M.elems . M.filterWithKey (\k _ -> "z" `L.isPrefixOf` k) $ (wires d)
+
+parse :: String -> Device
+parse input =
+  let [as, bs] = lineGroups input
+      vals = M.fromList $ map parseVal as
+      cons = map parseCon bs
+   in Device { connections = cons, wires = vals }
+  where
+    parseCon :: String -> Connection
+    parseCon str =
+      let [a, b, c, "->", d] = words str
+          gate = case b of
+                   "AND" -> AND
+                   "OR" -> OR
+                   "XOR" -> XOR
+       in Connection { gate = gate, input = (a, c), output = d }
+
+    parseVal :: String -> (String, Bool)
+    parseVal str =
+      let [name, v] = splitOnL ": " str
+          val = case v of
+                  "1" -> True
+                  "0" -> False
+       in (name, val)
---
+++ test/Day24Spec.hs
@@ -0,0 +1,85 @@
+module Day24Spec (spec) where
+
+import Day24
+import Test.Hspec hiding (example)
+
+example1, example2 :: String
+
+example1 = "x00: 1\n\
+           \x01: 1\n\
+           \x02: 1\n\
+           \y00: 0\n\
+           \y01: 1\n\
+           \y02: 0\n\
+           \\n\
+           \x00 AND y00 -> z00\n\
+           \x01 XOR y01 -> z01\n\
+           \x02 OR y02 -> z02\n"
+
+example2 = "x00: 1\n\
+           \x01: 0\n\
+           \x02: 1\n\
+           \x03: 1\n\
+           \x04: 0\n\
+           \y00: 1\n\
+           \y01: 1\n\
+           \y02: 1\n\
+           \y03: 1\n\
+           \y04: 1\n\
+           \\n\
+           \ntg XOR fgs -> mjb\n\
+           \y02 OR x01 -> tnw\n\
+           \kwq OR kpj -> z05\n\
+           \x00 OR x03 -> fst\n\
+           \tgd XOR rvg -> z01\n\
+           \vdt OR tnw -> bfw\n\
+           \bfw AND frj -> z10\n\
+           \ffh OR nrd -> bqk\n\
+           \y00 AND y03 -> djm\n\
+           \y03 OR y00 -> psh\n\
+           \bqk OR frj -> z08\n\
+           \tnw OR fst -> frj\n\
+           \gnj AND tgd -> z11\n\
+           \bfw XOR mjb -> z00\n\
+           \x03 OR x00 -> vdt\n\
+           \gnj AND wpb -> z02\n\
+           \x04 AND y00 -> kjc\n\
+           \djm OR pbm -> qhw\n\
+           \nrd AND vdt -> hwm\n\
+           \kjc AND fst -> rvg\n\
+           \y04 OR y02 -> fgs\n\
+           \y01 AND x02 -> pbm\n\
+           \ntg OR kjc -> kwq\n\
+           \psh XOR fgs -> tgd\n\
+           \qhw XOR tgd -> z09\n\
+           \pbm OR djm -> kpj\n\
+           \x03 XOR y03 -> ffh\n\
+           \x00 XOR y04 -> ntg\n\
+           \bfw OR bqk -> z06\n\
+           \nrd XOR fgs -> wpb\n\
+           \frj XOR qhw -> z04\n\
+           \bqk OR frj -> z07\n\
+           \y03 OR x01 -> nrd\n\
+           \hwm AND bqk -> z03\n\
+           \tgd XOR rvg -> z12\n\
+           \tnw OR pbm -> gnj\n"
+
+spec :: Spec
+spec = do
+  describe "part1" $ do
+    it "returns number on the Z-wires" $ do
+      part1 example1 `shouldBe` 4
+      part1 example2 `shouldBe` 2024
+
+  describe "part2" $ do
+    it "TBD" $ do
+      part2 example2 `shouldBe` 0
+
+  describe "answers" $ do
+    it "for part 1" $ do
+      input <- readFile "inputs/24_wires"
+      part1 input `shouldBe` 51837135476040
+
+    it "for part 2" $ do
+      input <- readFile "inputs/24_wires"
+      part2 input `shouldBe` 0
